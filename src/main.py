8cb82cf71b3120c0b1d36d065485ef45f8e4f7b9
MaksimSemenov-exe <arefbrq@gmail.com>
2026-09-05 18:39:36 +0300

    Функция create_database выполняется всегда, независимо от наличия файла БД. Обновляется uid в таблице last_mail после отправки сообщения пользователю

--- src/main.py
+++ src/main.py
@@ -8,25 +8,23 @@ from src.storage.db import Database
 from src.mail.imap import MailClient
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-dotenv_path = os.path.join(current_dir, '..', 'config.env')
+dotenv_path = os.path.join(current_dir, "..", "config.env")
 load_dotenv(dotenv_path=dotenv_path)
 
+
 def main():
-    """Получение токена, запуск бота, """
+    """Получение токена, запуск бота"""
     TOKEN = os.getenv("BOT_TOKEN")
     print(TOKEN)
 
     db = Database()
+    db.create_database()
+
     loop = asyncio.get_event_loop()
-
-
-    if not os.path.isfile(r'C:\Users\arefb\PycharmProjects\Telegram-Mail-Bridge\src\storage\mail.db'):
-        db.create_database()
 
     app = Application.builder().token(TOKEN).build()
     app.add_handler(conv_handler)
     app.add_handler(CommandHandler("help", help))
-
 
     def start_idle(user_data, loop):
         client = MailClient(
@@ -35,19 +33,23 @@ def main():
             password=user_data[2],
         )
         client.connect()
-        print('CONNECTED')
+        print("CONNECTED")
         chat_id = user_data[0]
 
         def handle_new_message(msg):
-            text = f'От: {msg['from']}\n Тема: {msg["subject"]}\n Текст: {msg['text']}'
+            db_local = Database()
+            text = f'От: {msg['from']}\nТема: {msg["subject"]}\nТекст: {msg['text']}'
             print(f"Отправка письма")
             asyncio.run_coroutine_threadsafe(app.bot.send_message(chat_id, text), loop)
+            db_local.update_uid(msg['uid'], client.username)
 
         client.idle(handle_new_message)
+
     users = db.get_all_users()
     print(users)
     for user in users:
-        threading.Thread(target=start_idle, args=(user, loop), daemon=True).start()
+        threading.Thread(target=start_idle, args=(user, loop), daemon=True
+            ).start()
 
     print("Бот запущен...")
     app.run_polling()
