7073f6f279c69d2a99e70230eb0f202c5d711ee7
MaksimSemenov-exe <arefbrq@gmail.com>
2026-09-07 11:08:15 +0300

    Вся постороння логика вынесена. Оставлен запуск бота и запуск IDLE-потоков для пользователей.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 import os
 import asyncio
-import threading
 from telegram.ext import Application, CommandHandler, Updater
 from src.bot.handlers import help, conv_handler
 from dotenv import load_dotenv
+from src.mail.MailManager import MailManager
 from src.storage.db import Database
-from src.mail.imap import MailClient
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 dotenv_path = os.path.join(current_dir, "..", "config.env")
@@ -13,7 +12,9 @@ load_dotenv(dotenv_path=dotenv_path)
 
 
 def main():
+
     """Получение токена, запуск бота"""
+
     TOKEN = os.getenv("BOT_TOKEN")
     print(TOKEN)
 
@@ -26,35 +27,11 @@ def main():
     app.add_handler(conv_handler)
     app.add_handler(CommandHandler("help", help))
 
-    def start_idle(user_data, loop):
-        client = MailClient(
-            server=user_data[3],
-            username=user_data[1],
-            password=user_data[2],
-        )
-        client.connect()
-        print("CONNECTED")
-        chat_id = user_data[0]
+    mail_manager = MailManager(app, loop)
 
-        def handle_new_message(msg):
-            db_local = Database()
-            text = f'От: {msg['from']}\nТема: {msg["subject"]}\nТекст: {msg['text']}'
-            print(f"Отправка письма")
-            asyncio.run_coroutine_threadsafe(app.bot.send_message(chat_id, text), loop)
-            db_local.update_uid(msg['uid'], client.username)
+    app.bot_data['mail_manager'] = mail_manager
 
-        client.idle(handle_new_message)
-
-    def start_idle_for_user(user_data, loop):
-        threading.Thread(target=start_idle, args=(user_data, loop), daemon=True).start()
-
-    app.bot_data['start_idle_for_user'] = start_idle_for_user
-
-    users = db.get_all_users()
-    print(users)
-    for user in users:
-        threading.Thread(target=start_idle, args=(user, loop), daemon=True
-            ).start()
+    mail_manager.start_idle_for_all_users()
 
     print("Бот запущен...")
     app.run_polling()
