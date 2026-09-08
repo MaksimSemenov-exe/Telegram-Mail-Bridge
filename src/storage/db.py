50404b85bbc02cccf0b79a445e2adebebad6a8eb
MaksimSemenov-exe <arefbrq@gmail.com>
2026-09-08 12:06:18 +0300

    Реализована функция для проверки работы IDLE-режима на основе записи в БД у конкретного пользователя

--- src/storage/db.py
+++ src/storage/db.py
@@ -78,3 +78,9 @@ class Database:
     def get_user_info(self, user_id):
         query = "SELECT * FROM users WHERE user_id = ?"
         self.cursor.execute(query, (user_id, ))
+
+    def is_active(self, email):
+        query = "SELECT is_active FROM users WHERE email = ?"
+        self.cursor.execute(query, (email, ))
+
+    
