9b6026199bfab7fee5a2d58d7df3f0268c2875ed
MaksimSemenov-exe <arefbrq@gmail.com>
2026-09-12 20:38:00 +0300

    Приведение кода к PEP-8

--- src/mail/MailManager.py
+++ src/mail/MailManager.py
@@ -11,7 +11,9 @@ class MailManager:
         self.threads = []
         self.db = Database()
 
-    def start_idle_for_user(self, server: str, username: str, password: str, chat_id: int) -> None:
+    def start_idle_for_user(
+        self, server: str, username: str, password: str, chat_id: int
+    ) -> None:
         client = MailClient(server, username, password)
         client.connect()
 
@@ -19,10 +21,12 @@ class MailManager:
             db_local = Database()
             text = f'От: {msg['from']}\nТема: {msg["subject"]}\nТекст: {msg['text']}'
             print(f"Отправка письма")
-            asyncio.run_coroutine_threadsafe(self.app.bot.send_message(chat_id, text), self.loop)
-            db_local.update_uid(msg['uid'], client.username)
+            asyncio.run_coroutine_threadsafe(
+                self.app.bot.send_message(chat_id, text), self.loop
+            )
+            db_local.update_uid(msg["uid"], client.username)
 
-        print('Idle открыт')
+        print("Idle открыт")
         client.idle(handle_new_message)
 
     def start_idle_for_all_users(self) -> None:
@@ -30,21 +34,28 @@ class MailManager:
         users = self.db.get_all_users()
 
         for user in users:
-            thread = threading.Thread(target=self.start_idle_for_user, args=(user[3], user[1], user[2], user[0]), daemon=True
-                ).start()
+            thread = threading.Thread(
+                target=self.start_idle_for_user,
+                args=(user[3], user[1], user[2], user[0]),
+                daemon=True,
+            ).start()
             self.threads.append(thread)
 
-    def start_thread_for_user(self, server: str, username: str, password: str, chat_id: int ) -> None:
+    def start_thread_for_user(
+        self, server: str, username: str, password: str, chat_id: int
+    ) -> None:
         thread = threading.Thread(
             target=self.start_idle_for_user,
             args=(server, username, password, chat_id),
-            daemon=True
+            daemon=True,
         ).start()
 
         self.threads.append(thread)
-        print('Thread открыт')
+        print("Thread открыт")
 
-    def manual_check(self, server: str, username: str, password: str, chat_id: int) -> None:
+    def manual_check(
+        self, server: str, username: str, password: str, chat_id: int
+    ) -> None:
         client = MailClient(server, username, password)
         db_local = Database()
         client.connect()
@@ -52,5 +63,7 @@ class MailManager:
 
         for msg in messages:
             text = f'От: {msg['from']}\nТема: {msg['subject']}\nТекст: {msg['text']}'
-            asyncio.run_coroutine_threadsafe(self.app.bot.send_message(chat_id, text), self.loop)
-            db_local.update_uid(msg['uid'], client.username)
+            asyncio.run_coroutine_threadsafe(
+                self.app.bot.send_message(chat_id, text), self.loop
+            )
+            db_local.update_uid(msg["uid"], client.username)
