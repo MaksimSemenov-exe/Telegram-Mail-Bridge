4ec72ab31d7c9f300a4a78e1709125659783c855
MaksimSemenov-exe <arefbrq@gmail.com>
2026-09-13 09:40:02 +0300

    Более детальная настройка логера для вывода сообщений уровня DEBUG в консоль

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -2,8 +2,15 @@ import logging
 
 
 def setup_logger():
+
+    file_handler = logging.FileHandler("src/logs/bot.log", encoding="utf-8")
+    file_handler.setLevel(logging.INFO)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.DEBUG)
+
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        handlers=[logging.FileHandler("src/logs/bot.log"), logging.StreamHandler()],
+        handlers=[file_handler, stream_handler],
     )
