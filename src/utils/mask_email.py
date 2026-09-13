4ffb0ff84bea0025e0e2548f9e7ae1c6f940d67e
MaksimSemenov-exe <arefbrq@gmail.com>
2026-09-13 12:04:03 +0300

    Исправил название переменной

--- src/utils/mask_email.py
+++ src/utils/mask_email.py
@@ -3,9 +3,9 @@ def mask_email(email):
     name, domain = email.split('@')
 
     if len(name) <= 2:
-        namee = name[0] + '*'
-        return f'{namee}@{domain}'
-    
+        name = name[0] + '*'
+        return f'{name}@{domain}'
+
     else:
         name = name[0] + '*' * (len(name) - 2) + name[-1]
         return f'{name}@{domain}'
