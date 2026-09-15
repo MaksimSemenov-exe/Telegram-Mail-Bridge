76bf5b6ed0cfc6804e817603f3d34348e51eef0b
MaksimSemenov-exe <arefbrq@gmail.com>
2026-09-15 21:33:16 +0300

    Реализована проверка, задан ли токен бота, а так же реализовано создание таблиц в БД при запуске бота и обработка исключений

--- src/main.py
+++ src/main.py
@@ -2,9 +2,14 @@ import os
 import asyncio
 from telegram.ext import Application, CommandHandler, Updater
 from src.bot.handlers import help, conv_handler
+from src.storage.db import Database
 from dotenv import load_dotenv
 from src.mail.MailManager import MailManager
 from src.utils.logger import setup_logger
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -17,8 +22,20 @@ def main():
     """Получение токена, запуск бота"""
 
     TOKEN = os.getenv("BOT_TOKEN")
-    print(TOKEN)
 
+    if not TOKEN:
+        logger.critical('BOT-токен не задан')
+        raise SystemExit(1)
+
+    db = Database()
+
+    setup_logger()
+
+    try:
+        db.create_database()
+    except Exception:
+        logger.critical('Не удалось создать БД, работа программы невозможна')
+        raise
     loop = asyncio.get_event_loop()
 
     app = Application.builder().token(TOKEN).build()
@@ -30,8 +47,6 @@ def main():
     app.bot_data['mail_manager'] = mail_manager
 
     mail_manager.start_idle_for_all_users()
-
-    setup_logger()
 
     print('Бот запущен')
     app.run_polling()
