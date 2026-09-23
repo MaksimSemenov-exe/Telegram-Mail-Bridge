4aeb5c5b397f96b075acb194bc9ccf587863a812
MaksimSemenov-exe <arefbrq@gmail.com>
2026-09-23 11:23:22 +0300

    Функция переделана под работу с JSON

--- src/utils/get_user_server.py
+++ src/utils/get_user_server.py
@@ -1,8 +1,29 @@
-from src.utils.imap_servers import imap_data
+import json
+import logging
+import os
+
+logger = logging.getLogger(__name__)
 
 
 def get_user_server(email: str) -> str:
     """Получение IMAP сервера пользователя по его почтовому адресу"""
-    server = imap_data[email.split("@")[1]].get("server")
-    return server
+
+    domain = email.split('@')[1]
+
+    base_dir = os.path.dirname(__file__)
+    json_path = os.path.join(base_dir, '..', 'configs', 'imap_config.json')
+    try:
+        with open(json_path, 'r') as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        logger.exception('Config-файл не найден path=%s', json_path)
+        raise
+
+    email_config = data.get(domain)
+    if email_config is None:
+        logger.warning('Не найдены записи о данном почтовом сервисе domain=%s', domain)
+        raise
+
+    return email_config.get('server')
+
 
