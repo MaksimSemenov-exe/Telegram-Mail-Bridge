0ed3bc59c5c9129234f5053d8ed13dfc9fef75c2
MaksimSemenov-exe <arefbrq@gmail.com>
2026-09-23 11:22:34 +0300

    Функция переделана под работу с JSON

--- src/utils/get_user_port.py
+++ src/utils/get_user_port.py
@@ -1,10 +1,32 @@
-from src.utils.imap_servers import imap_data
+import json
+import logging
+import os
+
+
+logger = logging.getLogger(__name__)
 
 
 def get_user_port(email: str) -> str:
-
     """Получение SSL порта пользователя по его почтовому адресу"""
-    server = imap_data[email.split("@")[1]].get("ssl_port")
-    return server
+
+    domain = email.split("@")[1]
+
+    base_dir = os.path.dirname(__file__)
+    json_path = os.path.join(base_dir, '..', 'configs', 'imap_config.json')
+
+    with open(json_path, 'r') as f:
+        data = json.load(f)
+
+    email_config = data.get(domain)
+    if email_config is None:
+        logger.warning('Не найдены записи о домене domain=%s', domain)
+        raise ValueError(f'Нет конфига для домена {domain}')
+
+    port = email_config.get('ssl_port')
+    if not port:
+        logger.warning("В конфиге нет port для domain=%s", domain)
+        raise ValueError(f"Нет port для домена {domain}")
+
+    return port
 
 
